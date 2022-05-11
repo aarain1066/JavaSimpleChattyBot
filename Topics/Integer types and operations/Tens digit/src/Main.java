8a92e24dab4df90bed78c018e3423907a7d741f8
Asim Arain <asimarain@Asims-MBP.lan>
2022-05-11 12:16:36 -0400

    removing collateral files

--- Topics/Integer types and operations/Tens digit/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        long originalNumber = scanner.nextLong();
-        System.out.println((originalNumber % 100) / 10 );
-    }
-}
